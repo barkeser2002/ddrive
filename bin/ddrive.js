f7d6c43c3622d09921287064b2d17cfc96374f60
Barış Keser <45911502+barkeser2002@users.noreply.github.com>
2023-01-31 00:05:54 +0000

    Fix

--- bin/ddrive.js
+++ bin/ddrive.js
@@ -2,15 +2,13 @@ const config = require('./config')()
 const { DFs, HttpServer } = require('../src')
 
 const startApp = async () => {
-const mySecret = process.env['DATABASE_URL']
-const mySecret = process.env['DATABASE_URL']
-    const { DFsConfig, httpConfig } = config
-    // Create DFs Instance
-    const dfs = new DFs(DFsConfig)
-    // Create HTTP Server instance
-    const httpServer = HttpServer(dfs, httpConfig)
+  const { DFsConfig, httpConfig } = config
+  // Create DFs Instance
+  const dfs = new DFs(DFsConfig)
+  // Create HTTP Server instance
+  const httpServer = HttpServer(dfs, httpConfig)
 
-    return httpServer.listen({ host: '0.0.0.0', port: httpConfig.port })
+  return httpServer.listen({ host: '0.0.0.0', port: httpConfig.port })
 }
 
 startApp().then()
